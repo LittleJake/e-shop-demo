6a0327e29a432074f6453666f73c8d50151d87e4
LittleJake <465917717@qq.com>
2019-02-22 14:38:07 +1100

    update

--- application/index/controller/User.php
+++ application/index/controller/User.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * Created by IntelliJ IDEA.
- * User: Sue Zhang
+ * User: LittleJake
  * Date: 2019/2/8
  * Time: 22:57
  */
@@ -13,21 +13,48 @@ class User extends Base
 {
     //登录
     public function loginAction(){
-
+		if($this->request->isPost())
+			return $this->success();
+		
+		$this->assign('page_title', '登录');
+		return $this->fetch();
     }
     //登出
     public function logoutAction(){
 		
+		return $this->success('登出成功', url('/'));
 	}
     //购物车
     public function cartAction(){
 		
 		
+		$this->assign('page_title', '购物车');
+		return $this->fetch();
+		
 	}
     //个人订单
-    public function orderAction(){}
+    public function orderAction(){
+		
+		$this->assign('page_title', '个人订单');
+		return $this->fetch();
+	}
     //地址
-    public function addressAction(){}
+    public function addressAction(){
+		
+		$this->assign('page_title', '地址列表');
+		return $this->fetch();
+	}
     //增加地址
-    public function addAddressAction(){}
+    public function addAddressAction(){
+		
+		$this->assign('page_title', '添加地址');
+		return $this->fetch();
+	}
+	//注册
+	public function regAction(){
+		
+		$this->assign('page_title', '注册');
+		return $this->fetch();
+	}
+	
 }
