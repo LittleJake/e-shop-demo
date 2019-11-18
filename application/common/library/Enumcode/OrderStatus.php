129041acc9ac756bd430faed86f8e516c382595e
LittleJake <465917717@qq.com>
2019-11-18 12:44:49 +0800

    添加订单常量

---
+++ application/common/library/Enumcode/OrderStatus.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: LittleJake
+ * Date: 2019/11/18
+ * Time: 12:38
+ */
+
+namespace app\common\library\Enumcode;
+
+
+class OrderStatus
+{
+    const ORDER_CLOSE = 0;
+    const ORDER_UNPAID = 2;
+    const ORDER_PAID = 1;
+    const ORDER_SHIPPING = 3;
+    const ORDER_NEED_COMMENT = 4;
+    const ORDER_FINISH = 5;
+}
