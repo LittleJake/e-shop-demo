6883a88ff569b848d8dcd3a5a81a3c6fb41b888a
LittleJake <465917717@qq.com>
2019-11-20 23:58:34 +0800

    添加redis支持

--- config/cache.php
+++ config/cache.php
@@ -15,11 +15,23 @@
 
 return [
     // 驱动方式
-    'type'   => 'File',
-    // 缓存保存目录
-    'path'   => '',
+    'type'   => 'Redis',
     // 缓存前缀
     'prefix' => '',
     // 缓存有效期 0表示永久缓存
     'expire' => 0,
+    //Redis host
+    'host'       => '127.0.0.1',
+    //Redis port
+    'port'       => 6379,
+    //Redis 连接密码
+    'password'   => '',
+    //Redis数据库选择
+    'select'     => 0,
+    //连接超时时间
+    'timeout'    => 0,
+    //持久化连接
+    'persistent' => false,
+    //数据序列化
+    'serialize'  => true,
 ];
