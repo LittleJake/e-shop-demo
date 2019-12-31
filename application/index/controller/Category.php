9cb33a07798254e57160cc210b40c28c9afba813
LittleJake <465917717@qq.com>
2019-12-31 13:30:45 +0800

    update category index

--- application/index/controller/Category.php
+++ application/index/controller/Category.php
@@ -14,8 +14,39 @@ namespace app\index\controller;
 
 class Category extends Common
 {
-    public function indexAction(){
+    public function indexAction($id = 0){
+        $modelCategory = new \app\common\model\Category();
+        $modelGood = new \app\common\model\Good();
+        $category = $modelCategory ->select();
+
+        $this->assign('category', $category);
+
+        if($id != 0){
+            if(empty($category)){
+                $this->assign('goods', []);
+            } else{
+                $goods = $modelGood -> where([
+                    'cate_id' => $id
+                ]) ->withMin('GoodCat', 'price') -> paginate(15);
+
+                $this->assign('goods', $goods);
+            }
+        }
+        else{
+            if(empty($category)){
+                $this->assign('goods', []);
+            } else{
+                $goods = $modelGood -> where([
+                    'cate_id' => $category[0]['id']
+                ]) ->withMin('GoodCat', 'price') -> paginate(15);
+
+                $this->assign('goods', $goods);
+            }
+
+        }
+
         $this->assign('page_title', "分类");
+        $this->assign('id', $id);
         return $this->fetch();
     }
 }
